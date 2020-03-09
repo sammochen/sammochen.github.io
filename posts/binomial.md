60fa7a32bc7ac033164ea7069ddd6cd2dcfe985f
sammochen <samuelshchen@gmail.com>
2020-03-10 07:59:38 +1300

    binomial

--- posts/binomial.md
+++ posts/binomial.md
@@ -4,5 +4,11 @@ title: binomial distribution
 src: ../src/binomial.js
 ---
 
+### how to use
+click to let the balls cascade through the circular obstacles, and click again to stop. the balls will automatically stop once it reaches a certain height to prevent overflow.
+
 ### description
-lmao
+this visualisation provides an intuitive way of understanding the binomial curve.
+at each level, the ball has a ~50/50 choice of going left or right, which results in an approximate binomial distribution at the bottom! 
+
+regarding the physics of the simulation, the balls do not interact with each other and collide only with the obstacles. the balls feel a repulsive force from the obstacles, and they stop reacting to forces once it settles at the bottom.
--- src/binomial.js
+++ src/binomial.js
@@ -8,78 +8,131 @@ canvas.style.border = "1px solid";
 
 parent.appendChild(canvas);
 
+function norm(a, b) {
+    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+}
+
 class ball {
     constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.dx = (Math.random() - 0.5) * 5;
-        this.dy = (Math.random() - 0.5) * 5;
+        this.x = canvas.width / 2;
+        this.y = -1;
+        this.r = 2;
+        this.dx = 0.01 * (Math.random() - 0.5);
+        this.dy = 0;
         this.ax = 0;
         this.ay = 0;
+        this.power = 1;
 
-        this.oldx = this.x;
-        this.oldy = this.y;
+        this.maxy = canvas.height + 100;
+    }
+
+    make(x,y,r,p) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.power = p;
+    }
+
+    hit(b) {
+        let dist = norm(b.x - this.x, b.y - this.y);
+        if (dist < (this.r + b.r)) {
+            let dx = (this.x - b.x) / dist;
+            let dy = (this.y - b.y) / dist;
+            let factor = b.power * 0.1 * (this.r + b.r - dist); 
+            
+            this.ax += factor * dx;
+            this.ay += factor * dy;            
+        }
     }
 }
 
 balls = [];
-init();
-go();
+hitters = [];
+let partitions = 12;
+let flow = true;
+let canflow = true;
 
+let freq = new Array(partitions+1);
+for (let i = 0; i < freq.length; i++) freq[i] = 0;
+
+init();
+
+canvas.onclick=toggle;
+function toggle() {
+    if (canflow) flow = !flow;
+}
+
+// initialise the grid of obstacles
 function init() {
-    for (let i = 0; i < 15; i++) {
-        balls.push(new ball());
+    let side = 25;
+    for (let i = 0; i < 6; i++) {
+        let x = canvas.width/2;
+        if (i%2==1) x += side/2;
+        while (x > 0) x -= side;
+        while (x < canvas.width) {
+            let b = new ball();
+            b.make(x, side * (i+1) * Math.sqrt(3) / 2, 7, 30);
+            hitters.push(b);
+            x += side;
+        }
+
     }
     paint();
+    go();
 }
 
 async function go() {
-    let wait = 0;
     while (true) {
-        if (wait == 40) {
+        if (flow) {
             balls.push(new ball());
-            wait = 0;
         }
-        wait++;
 
         // do forces
         for (let i = 0; i < balls.length; i++) {
-            balls[i].ay += 0.5
+            balls[i].ay += 1;
+            if (balls[i].maxy < canvas.height) continue;
+            for (let j = 0; j < hitters.length; j++) {
+                balls[i].hit(hitters[j]);
+            }
         }
 
         // act forces
         for (let i = 0; i < balls.length; i++) {
-            balls[i].oldx = balls[i].x;
-            balls[i].oldy = balls[i].y;
-
-            balls[i].dx += balls[i].ax;
-            balls[i].dy += balls[i].ay;
+            // friction
+            balls[i].dx *= 0.8;
+            balls[i].dy *= 0.9;
+            
+            // act forces
+            balls[i].dx += 0.8 * balls[i].ax;
+            balls[i].dy += 0.8 * balls[i].ay;
             balls[i].x += balls[i].dx;
             balls[i].y += balls[i].dy;
 
-            // friction
-            balls[i].dx *= 0.999;
-            balls[i].dy *= 0.999;
             // forces stop
             balls[i].ax = 0;
             balls[i].ay = 0;
 
             // check out of bounds
-            if (balls[i].y > canvas.height) {
-                balls[i].y = canvas.height;
+            if (balls[i].y + balls[i].r > balls[i].maxy) {
+                balls[i].y = balls[i].maxy-balls[i].r;
                 balls[i].dy *= -1;
             }
 
-            // check out of bounds
-            if (balls[i].x > canvas.width) {
-                balls[i].x = canvas.width;
-                balls[i].dx *= -1;
-            }
+            // add low walls
+            if (balls[i].y / canvas.height > 0.46) {
+                let index = Math.round(balls[i].x / (canvas.width / partitions));
+                balls[i].x = index * canvas.width / partitions;
+                balls[i].dx = 0;
 
-            // check out of bounds
-            if (balls[i].x < 0) {
-                balls[i].x = 1;
-                balls[i].dx *= -1;
+                if (balls[i].maxy > canvas.height) {
+                    freq[index]++;
+                    balls[i].maxy = canvas.height - 0.5*freq[index];
+
+                    if (balls[i].maxy < canvas.height * 0.53) {
+                        canflow = false;
+                        flow = false;
+                    }
+                }
             }
         }
 
@@ -92,16 +145,19 @@ function paint() {
     let ctx = canvas.getContext("2d");
     ctx.clearRect(0,0,canvas.width,canvas.height);
     for (let i = 0; i < balls.length; i++) {
-        drawCircle(balls[i].x, balls[i].y);
+        draw(balls[i], "#FF8C61");
+    }
+    for (let i = 0; i < hitters.length; i++) {
+        draw(hitters[i], "#EEEEEE");
     }
 }
 
-function drawCircle(x,y) {
+function draw(ball, style) {
     let ctx = canvas.getContext("2d");
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#6457A6";
+    ctx.lineWidth = 0;
+    ctx.strokeStyle = style;
     ctx.beginPath();
-    ctx.arc(x, y, 1, 0, 2 * Math.PI);
+    ctx.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI);
     ctx.stroke();
 }
 
